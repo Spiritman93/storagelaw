28907b0094035abd4bef2273ae46db804f9aa689
Spiriman93 <holms.12@mail.ru>
2022-02-02 00:04:43 +0300

    interface is connected

--- rest/src/main/java/com/lawstorage/restapp/RestControllerStorage.java
+++ rest/src/main/java/com/lawstorage/restapp/RestControllerStorage.java
@@ -4,13 +4,14 @@ package com.lawstorage.restapp;
 import com.lawstorage.law.Law;
 import com.lawstorage.repository.LawstorageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-public class RestControllerLawstorage {
+public class RestControllerStorage {
 
-    @Autowired
+
     LawstorageRepository lawstorageRepository;
 
     @GetMapping("/startpage")
